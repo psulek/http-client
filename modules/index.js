0c840fa8411f06f4f4c560c5841f2bef2f7e1a5c
Peter Šulek <peter.sulek@synotgames.com>
2018-08-31 13:15:41 +0200

    Added option sendContentLength

--- modules/index.js
+++ modules/index.js
@@ -3,37 +3,41 @@ import byteLength from 'byte-length'
 
 const global = (1, eval)('this')
 
+export const Options = {
+    sendContentLength: true
+}
+
 const stringifyQuery = (query) =>
-  (typeof query === 'string' ? query : stringify(query))
+    (typeof query === 'string' ? query : stringify(query))
 
 const stringifyJSON = (json) =>
-  (typeof json === 'string' ? json : JSON.stringify(json))
+    (typeof json === 'string' ? json : JSON.stringify(json))
 
 const processResponse = (response, handlers) =>
-  handlers.reduce(
-    (promise, handler) => promise.then(handler),
-    Promise.resolve(response)
-  )
+    handlers.reduce(
+        (promise, handler) => promise.then(handler),
+        Promise.resolve(response)
+    )
 
 /**
  * Returns a new fetch function that knows how to execute
  * options.responseHandlers on the response.
  */
 export const enableRecv = (fetch) =>
-  (input, options = {}) =>
-    fetch(input, options).then(response => {
-      const responseHandlers = options.responseHandlers
+    (input, options = {}) =>
+        fetch(input, options).then(response => {
+            const responseHandlers = options.responseHandlers
 
-      return (responseHandlers && responseHandlers.length)
-        ? processResponse(response, responseHandlers)
-        : response
-    })
+            return (responseHandlers && responseHandlers.length)
+                ? processResponse(response, responseHandlers)
+                : response
+        })
 
 // Deprecated.
 export const enhanceFetch = enableRecv
 
 const emptyStack = (fetch, input, options) =>
-  fetch(input, options)
+    fetch(input, options)
 
 /**
  * Creates a middleware "stack" function using all arguments.
@@ -43,18 +47,18 @@ const emptyStack = (fetch, input, options) =>
  * stacks with different characteristics.
  */
 export const createStack = (...middleware) => {
-  if (middleware.length === 0)
-    return emptyStack
+    if (middleware.length === 0)
+        return emptyStack
 
-  return middleware.reduceRight(
-    (inner, outer) =>
-      (fetch, outerInput, outerOptions) =>
-        outer(
-          (innerInput, innerOptions) => inner(fetch, innerInput, innerOptions),
-          outerInput,
-          outerOptions
-        )
-  )
+    return middleware.reduceRight(
+        (inner, outer) =>
+            (fetch, outerInput, outerOptions) =>
+                outer(
+                    (innerInput, innerOptions) => inner(fetch, innerInput, innerOptions),
+                    outerInput,
+                    outerOptions
+                )
+    )
 }
 
 /**
@@ -72,15 +76,15 @@ export const createStack = (...middleware) => {
  * when you just want to use the global fetch function.
  */
 export const createFetch = (...middleware) => {
-  if (middleware.length === 0)
-    return global.fetch
+    if (middleware.length === 0)
+        return global.fetch
 
-  const stack = createStack(...middleware)
+    const stack = createStack(...middleware)
 
-  return enableRecv(
-    (input, options = {}) =>
-      stack(global.fetch, input, options)
-  )
+    return enableRecv(
+        (input, options = {}) =>
+            stack(global.fetch, input, options)
+    )
 }
 
 // Deprecated.
@@ -91,106 +95,107 @@ export { mainFetch as fetch }
  * Sets a property name and value in the options object.
  */
 export const init = (propertyName, value) =>
-  (fetch, input, options = {}) => {
-    options[propertyName] = value
-    return fetch(input, options)
-  }
+    (fetch, input, options = {}) => {
+        options[propertyName] = value
+        return fetch(input, options)
+    }
 
 /**
  * Sets the request method.
  */
 export const method = (verb) =>
-  init('method', verb)
+    init('method', verb)
 
 const setHeader = (options, name, value) => {
-  (options.headers || (options.headers = {}))[name] = value
+    (options.headers || (options.headers = {}))[name] = value
 }
 
 /**
  * Adds a header to the request.
  */
 export const header = (name, value) =>
-  (fetch, input, options = {}) => {
-    setHeader(options, name, value)
-    return fetch(input, options)
-  }
+    (fetch, input, options = {}) => {
+        setHeader(options, name, value)
+        return fetch(input, options)
+    }
 
 /**
  * Adds an Authorization header to the request.
  */
 export const auth = (value) =>
-  header('Authorization', value)
+    header('Authorization', value)
 
 /**
  * Adds an Accept header to the request.
  */
 export const accept = (value) =>
-  header('Accept', value)
+    header('Accept', value)
 
 /**
  * Adds the given string at the front of the request URL.
  */
 export const base = (baseURL) =>
-  (fetch, input, options) =>
-    fetch(baseURL + (input || ''), options)
+    (fetch, input, options) =>
+        fetch(baseURL + (input || ''), options)
 
 /**
  * Adds the given object to the query string in the request.
  */
 export const query = (object) => {
-  const queryString = stringifyQuery(object)
+    const queryString = stringifyQuery(object)
 
-  return (fetch, input, options) =>
-    fetch(input + (input.indexOf('?') === -1 ? '?' : '&') + queryString, options)
+    return (fetch, input, options) =>
+        fetch(input + (input.indexOf('?') === -1 ? '?' : '&') + queryString, options)
 }
 
 /**
  * Adds the given content to the request.
  */
 export const body = (content, contentType) =>
-  (fetch, input, options = {}) => {
-    options.body = content
+    (fetch, input, options = {}) => {
+        options.body = content
 
-    if (content.length != null)
-      setHeader(options, 'Content-Length', byteLength(content))
+        if (content.length != null && Options.sendContentLength) {
+            setHeader(options, 'Content-Length', byteLength(content))
+        }
 
-    if (contentType)
-      setHeader(options, 'Content-Type', contentType)
+        if (contentType)
+            setHeader(options, 'Content-Type', contentType)
 
-    return fetch(input, options)
-  }
+        return fetch(input, options)
+    }
 
 /**
  * Adds an application/json payload to the request.
  */
 export const json = (object) =>
-  body(stringifyJSON(object), 'application/json')
+    body(stringifyJSON(object), 'application/json')
 
 /**
  * Adds the given object to the query string of GET/HEAD requests
  * and as a application/x-www-form-urlencoded payload on all others.
  */
 export const params = (object) => {
-  const queryString = stringifyQuery(object)
+    const queryString = stringifyQuery(object)
 
-  return (fetch, input, options = {}) => {
-    const verb = (options.method || 'GET').toUpperCase()
-    const middleware = (verb === 'GET' || verb === 'HEAD')
-      ? query(queryString)
-      : body(queryString, 'application/x-www-form-urlencoded')
+    return (fetch, input, options = {}) => {
+        const verb = (options.method || 'GET').toUpperCase()
+        const middleware = (verb === 'GET' || verb === 'HEAD')
+            ? query(queryString)
+            : body(queryString, 'application/x-www-form-urlencoded')
 
-    return middleware(fetch, input, options)
-  }
+        return middleware(fetch, input, options)
+    }
 }
 
 /**
  * A helper for creating middleware that handles a successful response.
  */
 export const recv = (handler) =>
-  (fetch, input, options = {}) => {
-    (options.responseHandlers || (options.responseHandlers = [])).push(handler)
-    return fetch(input, options)
-  }
+    (fetch, input, options = {}) => {
+        (options.responseHandlers || (options.responseHandlers = [])).push(handler)
+        return fetch(input, options)
+    }
 
 // Deprecated.
 export const handleResponse = recv
@@ -201,25 +206,25 @@ export const onResponse = recv
  * using the given parser, and adds the result to response.body.
  */
 export const parse = (parser, as = 'body') =>
-  recv(response => {
-    if (as in response)
-      return response[as]
+    recv(response => {
+        if (as in response)
+            return response[as]
 
-    return response[parser]().then(body => {
-      response[as] = body
-      return response
-    }, error => {
-      throw new Error(`parse('${parser}') error: ${error.stack}`)
+        return response[parser]().then(body => {
+            response[as] = body
+            return response
+        }, error => {
+            throw new Error(`parse('${parser}') error: ${error.stack}`)
+        })
     })
-  })
 
 // Deprecated.
 export const parseJSON = (propertyName = 'jsonData') =>
-  parse('json', propertyName)
+    parse('json', propertyName)
 
 // Deprecated.
 export const parseText = (propertyName = 'textString') =>
-  parse('text', propertyName)
+    parse('text', propertyName)
 
 /**
  * Adds a debug property to the response/error that contains
@@ -227,26 +232,26 @@ export const parseText = (propertyName = 'textString') =>
  * testing and debugging.
  */
 export const debug = () =>
-  (fetch, input, options) =>
-    fetch(input, options)
-      .then(response => {
-        response.debug = { input, options }
-        return response
-      }, (error = new Error) => {
-        error.debug = { input, options }
-        throw error
-      })
+    (fetch, input, options) =>
+        fetch(input, options)
+            .then(response => {
+                response.debug = { input, options }
+                return response
+            }, (error = new Error) => {
+                error.debug = { input, options }
+                throw error
+            })
 
 // Deprecated.
 export const requestInfo = () =>
-  (fetch, input, options) =>
-    fetch(input, options)
-      .then(response => {
-        response.requestInput = input
-        response.requestOptions = options
-        return response
-      }, (error = new Error) => {
-        error.requestInput = input
-        error.requestOptions = options
-        throw error
-      })
+    (fetch, input, options) =>
+        fetch(input, options)
+            .then(response => {
+                response.requestInput = input
+                response.requestOptions = options
+                return response
+            }, (error = new Error) => {
+                error.requestInput = input
+                error.requestOptions = options
+                throw error
+            })
